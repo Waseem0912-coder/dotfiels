b8ad678d943256d5c1ee1585b0d44b2a9a687a5e
Waseem0912-coder <58933410+Waseem0912-coder@users.noreply.github.com>
2020-10-06 16:56:59 +0530

    all done

---
+++ backup/c.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib>
+#include <math.h>
+#include <string.h>
+
+void fun1(){
+
+
+
+}
+int main1()
+{
+
+
+
+
+
+}
+int main(){
+
+
+return 0;
+}
---
+++ backup/file.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+
+int main() {
+	
+	
+
+
+
+	return 0;
+}
